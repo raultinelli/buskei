d14ac71d8f92fd039659d2066388ef46b0c7f8a5
Raul Tinelli <raul.tinelli@gmail.com>
2020-11-16 17:49:19 -0300

    corrigindo long e lat

--- src/pages/ProfissionaisMap.tsx
+++ src/pages/ProfissionaisMap.tsx
@@ -4,7 +4,25 @@ import '../styles/pages/profissional-map.css';
 import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
+async function LocalDoUsuario() {
+    if ('geolocation' in navigator) {
+       await navigator.geolocation.getCurrentPosition((position) => {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            const posicao = [latitude, longitude];
+            console.log(posicao);
+
+            return posicao;
+        });
+    } else {
+        console.log("não passou if");
+    }
+}
+
+LocalDoUsuario();
+
 function ProfissionaisMap() {
+
     return(
         <div id="page-map">
             <aside>
@@ -18,7 +36,7 @@ function ProfissionaisMap() {
                     <span>Apollo</span>
                 </footer>
             </aside>
-            <MapContainer center={[-15.7193229,-55.3860335]} zoom={15} style={{ width: '100%', height: '100%' }}>
+            <MapContainer center={[-19.7414837,-40.6651371]} zoom={15} style={{ width: '100%', height: '100%' }}>
                 {/*<TileLayer
                     url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />*/}
