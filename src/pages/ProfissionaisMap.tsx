ed12ce0fb7812fd79f11ba00296f84b231297850
Raul Tinelli <raul.tinelli@gmail.com>
2020-11-17 10:19:59 -0300

    adição do ponteiro

--- src/pages/ProfissionaisMap.tsx
+++ src/pages/ProfissionaisMap.tsx
@@ -1,27 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from '../images/Logo.svg';
+import iconpointer from '../images/iconpointer.svg';
 import '../styles/pages/profissional-map.css';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import Leaflet from "leaflet";
 import 'leaflet/dist/leaflet.css';
 
-async function LocalDoUsuario() {
-    if ('geolocation' in navigator) {
-       await navigator.geolocation.getCurrentPosition((position) => {
-            const latitude = position.coords.latitude;
-            const longitude = position.coords.longitude;
-            const posicao = [latitude, longitude];
-            console.log(posicao);
 
-            return posicao;
-        });
-    } else {
-        console.log("não passou if");
-    }
-}
+const initialPosition = { lat: -14.133648, lng: -57.2233902 };
 
-LocalDoUsuario();
+const mapPinIcon = Leaflet.icon({
+    iconUrl: iconpointer,
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2],
+});
+
 
 function ProfissionaisMap() {
+
+
+    const [location, setLocation] = useState(initialPosition);
+
+    
+    if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            setLocation({
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            });
+        });
+    } else {
+            console.log("não passou if");
+    }
+
 
     return(
         <div id="page-map">
@@ -29,20 +41,24 @@ function ProfissionaisMap() {
                 <header>
                     <img src={logo} alt="logo"/>
                     <h2>Escolha um profissional no mapa</h2>
-                    <p>Para mais informações, entre em contato pelo WhatsApp do profissional.</p>
+                    <p>Clique no ponteiro de marcação para ter mais informações</p>
                 </header>
                 <footer>
                     <strong>Criado por</strong>
                     <span>Apollo</span>
                 </footer>
             </aside>
-            <MapContainer center={[-19.7414837,-40.6651371]} zoom={15} style={{ width: '100%', height: '100%' }}>
+            <MapContainer center={location} zoom={4.75} style={{ width: '100%', height: '100%' }}>
                 {/*<TileLayer
                     url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />*/}
                 <TileLayer
                     url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                 />
+                <Marker
+                     icon={mapPinIcon}
+                     position={[-19.7414837,-40.6651371]}
+                ></Marker>
             </MapContainer>
         </div>
     );
